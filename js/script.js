9c4a861bbc7861ff76202712c4238dc21826dcea
Karol Dąbrowski <dabrowski.carl@gmail.com>
2018-03-28 13:53:03 +0200

    Fix script

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 const span = $('span');
-    span.each(function(index, element) {
-});
+
 $('span:even').css('color', 'red');
 const paragraphs = $('p');
-paragraphs.each(function(index, element) {
+paragraphs.each(function (index, element) {
     var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
     $(element).append(button);
 });
-$('button').click(function(){
-	alert($(this).attr('data-tmp'));
+$('button').click(function () {
+    alert($(this).attr('data-tmp'));
 });
